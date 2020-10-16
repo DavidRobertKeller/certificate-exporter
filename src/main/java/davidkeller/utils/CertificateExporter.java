e98a5da5e22b8e1fe9f97838cc8d3db981a48ef3
unknown <david.keller.fr@gmail.com>
2020-10-16 14:38:05 +0200

    simple extraction

---
+++ src/main/java/davidkeller/utils/CertificateExporter.java
@@ -0,0 +1,28 @@
+package davidkeller.utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.security.KeyStore;
+import java.util.Enumeration;
+
+import java.security.cert.X509Certificate;
+
+
+public class CertificateExporter {
+	
+	public static void main(String[] args) throws Exception {
+		KeyStore keystore = KeyStore.getInstance("Windows-MY");
+		keystore.load(null, null); 
+		Enumeration<String> aliases = keystore.aliases();
+		
+        while(aliases.hasMoreElements()){
+            String alias = aliases.nextElement();
+        	File file = new File("./" + alias + ".crt");
+            System.out.println("alias "  + alias + ", public certificate saved in " + file.getAbsolutePath());
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+            	X509Certificate certificate = (X509Certificate) keystore.getCertificate(alias);
+            	fos.write(certificate.getEncoded());
+        	}
+        }
+	}
+}
