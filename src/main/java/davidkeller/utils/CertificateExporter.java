23eacca4e505832f68c7dd8d8d25c8f01750b3bc
unknown <david.keller.fr@gmail.com>
2020-10-16 14:39:01 +0200

    code indentation

--- src/main/java/davidkeller/utils/CertificateExporter.java
+++ src/main/java/davidkeller/utils/CertificateExporter.java
@@ -7,22 +7,21 @@ import java.util.Enumeration;
 
 import java.security.cert.X509Certificate;
 
-
 public class CertificateExporter {
-	
+
 	public static void main(String[] args) throws Exception {
 		KeyStore keystore = KeyStore.getInstance("Windows-MY");
-		keystore.load(null, null); 
+		keystore.load(null, null);
 		Enumeration<String> aliases = keystore.aliases();
-		
-        while(aliases.hasMoreElements()){
-            String alias = aliases.nextElement();
-        	File file = new File("./" + alias + ".crt");
-            System.out.println("alias "  + alias + ", public certificate saved in " + file.getAbsolutePath());
-            try (FileOutputStream fos = new FileOutputStream(file)) {
-            	X509Certificate certificate = (X509Certificate) keystore.getCertificate(alias);
-            	fos.write(certificate.getEncoded());
-        	}
-        }
+
+		while (aliases.hasMoreElements()) {
+			String alias = aliases.nextElement();
+			File file = new File("./" + alias + ".crt");
+			System.out.println("alias " + alias + ", public certificate saved in " + file.getAbsolutePath());
+			try (FileOutputStream fos = new FileOutputStream(file)) {
+				X509Certificate certificate = (X509Certificate) keystore.getCertificate(alias);
+				fos.write(certificate.getEncoded());
+			}
+		}
 	}
 }
